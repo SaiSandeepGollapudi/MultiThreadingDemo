1aee28daa3a1536c2331c4a62c7a81e485342d54
SaiSandeepGollapudi <gsandeep2406@gmail.com>
2024-03-31 14:08:36 -0400

    MultiThreading

--- src/main/java/org/example/ThreadServiceExample.java
+++ src/main/java/org/example/ThreadServiceExample.java
@@ -12,8 +12,18 @@ public class ThreadServiceExample {
 
         System.out.println("Name of thread: "+ Thread.currentThread().getName());
 
-        MyThread mythread= new MyThread();
-        mythread.start();
+        MyThread myThread1= new MyThread();
+        myThread1.start();
+
+        MyThread myThread2= new MyThread();
+        myThread2.start();
+
+        MyThread myThread3= new MyThread();
+        myThread3.start();
+
+        myThread1.join();// telling main method to wait for thread1 to join it and then only move
+        myThread2.join();
+        myThread3.join();
 
         long end = System.currentTimeMillis();
         System.out.println("Time taken: "+ (end-start));
